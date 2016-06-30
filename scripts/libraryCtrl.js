c1f7c47529896429b69ad67476c3812396fd17ab
Micro <maglars2@gmail.com>
2016-06-30 10:47:27 +0200

    Update libraryCtrl.js

--- scripts/libraryCtrl.js
+++ scripts/libraryCtrl.js
@@ -4,10 +4,30 @@ angular.module('scLibraryApp', []).controller('libraryCtrl', function($scope) {
 	var artists = [];
 
 	$scope.init = function() {
-		artists.push({"name": "Da Tweekaz", "url": "https://soundcloud.com/datweekaz", "country": "Norway"});
+		artists = [
+			{"name": "Da Tweekaz", "url": "https://soundcloud.com/datweekaz", "country": "Norway"},
+			{"name":"A V I 8","url":"https://soundcloud.com/avi8music", "country":"Scotland"},
+			{"name":"AeroliteDj","url":"https://soundcloud.com/aerolitedj","country":"United States"},
+			{"name":"Audiofreq","url":"https://soundcloud.com/audiofreqdj","country":"Netherlands"},
+			{"name":"DJ KEIJI","url":"https://soundcloud.com/keijidj","country":"United States"},
+			{"name":"Rechargeofficial","url":"https://soundcloud.com/rechargeofficial","country":"Netherlands"},
+			{"name":"Refuzion","url":"https://soundcloud.com/refuzion", "country": "Norway"},
+			{"name":"Syren","url":"https://soundcloud.com/syrendjmusic","country":"Australia"},
+			{"name":"Timekeeperz","url":"https://soundcloud.com/federico-tensini","country":"Italy"},
+			{"name":"Atmozfears","url":"https://soundcloud.com/atmozfears", "country":"Netherlands"},
+			{"name":"Cyber","url":"https://soundcloud.com/officialdjcyber", "country":"Switzerland"},
+			{"name":"Devin Wild","url":"https://soundcloud.com/devinwildmusic", "country":"Netherlands"},
+			{"name":"Distance","url":"https://soundcloud.com/distanceofficial","country":"Poland"},
+			{"name":"Firelite","url":"https://soundcloud.com/firelitemusic","country":"Australia"},
+			{"name":"Navion","url":"https://soundcloud.com/navion-2","country":"Netherlands"},
+			{"name":"Rebourne","url":"https://soundcloud.com/rebournemedia","country":"Netherlands"},
+			{"name":"The Vision","url":"https://soundcloud.com/thevisionofficial","country":"Netherlands"},
+			{"name":"Neilio","url":"https://soundcloud.com/djneilio","country":"Ireland"},
+			{"name":"Noiseshock","url":"https://soundcloud.com/noiseshock","country":"Argentina"},
+		];
 	}
 	
-    $scope.sortBy = function(newKey) {
+    	$scope.sortBy = function(newKey) {
 		reverse = (newKey === key) ? !reverse : false;
 		key = newKey;
 	};
